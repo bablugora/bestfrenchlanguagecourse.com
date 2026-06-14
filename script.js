18747a125241e2de39f2b9fbbd0e77f696af6d3f
Bablu Gora <bablugora4@gmail.com>
2026-06-14 11:09:15 +0530

    new

---
+++ script.js
@@ -0,0 +1,9 @@
+// const mydibba = document.getElementById("header-one");
+// let mybutton = document.querySelector("button");
+// mybutton.onclick = function(){
+// if (mybutton.style.backgroundColor !=="red";)
+// {mybutton.style.backgroundColor="red";}
+// else{mybutton.style.backgroundColor="green";}
+// 
+// };
+
